1db64a66f6c7fb0f39662df94da6f50a67ac4aef
Damanjeet Singh <damanjeetsingh434@gmail.com>
2025-09-26 19:13:46 +0530

    fixed terms-and-service page
    Signed-off-by: Damanjeet Singh <damanjeetsingh434@gmail.com>

--- src/app/(wrapper)/(extras)/terms-service/page.tsx
+++ src/app/(wrapper)/(extras)/terms-service/page.tsx
@@ -1,104 +1,210 @@
-// import React from "react";
+export default function page() {
+  return (
+    <>
+      <section className="max-w-[1800px] m-auto py-8 px-4 md:px-16 flex flex-col  ">
+        <h4 className="font-[area] text-[1.125rem] font-bold">
+          Home • Terms of Service
+        </h4>
+        <div className="max-w-[1000px] w-[60%]  m-auto ">
+          <h2 className="text-[2rem] sm:text-[2.25rem] lg:text-[2.75rem] font-[editorial] mb-5">
+            Terms of Service
+          </h2>
+          <h2 className="text-[1rem] font-[area] font-extrabold mt-4 ">
+            1. Introduction
+          </h2>
+          <p className="mt-2 font-[area] text-[1rem]">
+            Welcome to Two Zero Club (“we”, “us”, “our”). These Terms and
+            Conditions (“Terms”) govern your access to and use of our website
+            (the “Site”) and any products or services offered through it (the
+            “Service”).
+            <br />
+            <br />
+            By using our Site, creating an account, or purchasing from us, you
+            confirm that you accept these Terms. If you do not agree, you must
+            not use the Site.
+            <br />
+            <br />
+            We may update these Terms from time to time by posting changes on
+            this page. Continued use of the Site constitutes acceptance of the
+            revised Terms.
+          </p>
+          <h2 className="text-[1rem] font-[area] font-extrabold mt-4">
+            2. Other Applicable Terms
+          </h2>
+          <p className="mt-2 font-[area] text-[1rem] ">
+            These Terms incorporate the following additional policies, which
+            apply to your use of our Site and Services:
+          </p>
+          <ul className="list-disc ml-6 font-[area] text-[1rem]">
+            <li>
+              Our Privacy Policy (how we collect, use and protect your data).
+            </li>
+            <li>Our Returns & Refunds Policy.</li>
+            <li>
+              Any specific terms for promotions, events, or other services we
+              provide.
+            </li>
+          </ul>
 
-export default function TermsServicePage() {
-  return (<>
-    <div className="py-4 px-40 text-sm text-muted-foreground">
-     <p className="font-[area] font-[editorial] font-bold text-2xl">Home   •   Terms of Service</p>
-    </div>
-    <div className="max-w-3xl mx-auto p-6 text-muted-foreground">
-      <h1 className="text-4xl font-bold mb-6">Terms of Service</h1>
+          <h2 className="text-[1rem] font-[area] font-extrabold mt-4">
+            3. Eligibility & Accounts
+          </h2>
+          <ul className="list-disc ml-6 mt-2 font-[area] text-[1rem]">
+            <li>
+              You must be at least the age of majority in your country of
+              residence to use our Site or make purchases.
+            </li>
+            <li>
+              You are responsible for providing accurate, current, and complete
+              account and billing information.
+            </li>
+            <li>
+              You must keep your login credentials secure and not share them
+              with third parties.
+            </li>
+            <li>
+              We may suspend or terminate your account if you breach these
+              Terms.
+            </li>
+          </ul>
 
-      <h2 className="text-xl font-semibold mt-4">1. Introduction</h2>
-      
-      <p className="mt-2 font-[area] font-[editorial]">
-        Welcome to Two Zero Club (“we”, “us”, “our”). These Terms and Conditions (“Terms”) govern your access to and use of our website (the “Site”) and any products or services offered through it (the “Service”).
-<br /><br />
-By using our Site, creating an account, or purchasing from us, you confirm that you accept these Terms. If you do not agree, you must not use the Site.
-<br /><br />
-We may update these Terms from time to time by posting changes on this page. Continued use of the Site constitutes acceptance of the revised Terms.
+          <h2 className="text-[1rem] font-[area] font-extrabold mt-4">
+            4. Purchases
+          </h2>
+          <ul className="list-disc ml-6 mt-2 font-[area] text-[1rem]">
+            <li>
+              By placing an order, you agree to provide valid payment
+              information and authorize us to charge your payment method.
+            </li>
+            <li>
+              We reserve the right to refuse or cancel orders at our discretion,
+              including in cases of suspected fraud, errors in pricing, or
+              product unavailability.
+            </li>
+            <li>
+              All prices are subject to change without notice. Taxes, shipping
+              fees, and duties may apply depending on your location.
+            </li>
+          </ul>
 
-      </p>
+          <h2 className="text-[1rem] font-[area] font-extrabold mt-4">
+            5. Shipping & Delivery
+          </h2>
+          <ul className="list-disc ml-6 mt-2 font-[area] text-[1rem]">
+            <li>
+              We ship worldwide using trusted couriers. Shipping costs and
+              delivery times are displayed at checkout
+            </li>
+            <li>
+              Local taxes and import duties may apply and are the {`customer’s`}
+              responsibility.
+            </li>
+            <li>
+              We are not liable for delays caused by customs, courier errors, or
+              events outside our control.
+            </li>
+            <li>
+              Risk of loss passes to you once the order is delivered to the
+              carrier
+            </li>
+          </ul>
 
-      <h2 className="text-xl font-semibold mt-4">2. Other Applicable Terms</h2> 
-      <p className="mt-2 font-[area] font-[editorial]">
-        These Terms incorporate the following additional policies, which apply to your use of our Site and Services:
-      </p>
-      <ul className="list-disc ml-6 font-[area] font-[editorial]">
-        <li>Our Privacy Policy (how we collect, use and protect your data).</li>
-        <li>Our Returns & Refunds Policy.</li>
-        <li>Any specific terms for promotions, events, or other services we provide.</li>
-      </ul>
+          <h2 className="text-[1rem] font-[area] font-extrabold mt-4">
+            6. Returns & Refunds
+          </h2>
+          <ul className="list-disc ml-6 mt-2 font-[area] text-[1rem]">
+            <li>
+              You may return eligible items within 30 days of delivery, provided
+              they are unused, in original condition, and in original packaging.
+            </li>
+            <li>
+              Return shipping costs are your responsibility. If returned items
+              arrive damaged, only partial refunds may be issued
+            </li>
+            <li>
+              Damaged or defective items must be reported within 7 days of
+              delivery with supporting photos. Replacements will be offered
+              where possible.
+            </li>
+          </ul>
 
-      <h2 className="text-xl font-semibold mt-4">3. Eligibility & Accounts</h2> 
-      <ul className="list-disc ml-6 mt-2 font-[area] font-[editorial]">
-        <li>You must be at least the age of majority in your country of residence to use our Site or make purchases.</li>
-        <li>You are responsible for providing accurate, current, and complete account and billing information.</li>
-        <li>You must keep your login credentials secure and not share them with third parties.</li>
-        <li>We may suspend or terminate your account if you breach these Terms.</li>
-      </ul>
+          <h2 className="text-[1rem] font-[area] font-extrabold mt-4">
+            7. Intellectual Property
+          </h2>
+          <ul className="list-disc ml-6 mt-2 font-[area] text-[1rem]">
+            <li>
+              All intellectual property rights in the Site, products, branding,
+              and original content are owned by Two Zero Club or our licensors.
+            </li>
+            <li>
+              You may not copy, reproduce, distribute, or use any part of our
+              Site or products for commercial purposes without prior written
+              cosent.
+            </li>
+          </ul>
 
-      <h2 className="text-xl font-semibold mt-4">4. Purchases</h2> 
-      <ul className="list-disc ml-6 mt-2 font-[area] font-[editorial]">
-        <li>By placing an order, you agree to provide valid payment information and authorize us to charge your payment method.</li>
-        <li>We reserve the right to refuse or cancel orders at our discretion, including in cases of suspected fraud, errors in pricing, or product unavailability.</li>
-        <li>All prices are subject to change without notice. Taxes, shipping fees, and duties may apply depending on your location.</li>
-      </ul>
+          <h2 className="text-[1rem] font-[area] font-extrabold mt-4">
+            8. Prohibited Uses
+          </h2>
+          <ul className="list-disc ml-6 mt-2 font-[area] text-[1rem]">
+            <li>Violate any laws or regulations.</li>
+            <li>Transmit viruses, malware, or harmful code.</li>
+            <li>Infringe on intellectual property rights.</li>
+            <li>Harass, abuse, or defame others.</li>
+            <li>Misuse promotions, referral programs, or discounts.</li>
+          </ul>
 
-      <h2 className="text-xl font-semibold mt-4">5. Shipping & Delivery</h2> 
-      <ul className="list-disc ml-6 mt-2 font-[area] font-[editorial]">
-        <li>We ship worldwide using trusted couriers. Shipping costs and delivery times are displayed at checkout</li>
-        <li>Local taxes and import duties may apply and are the customer’s responsibility.</li>
-        <li>We are not liable for delays caused by customs, courier errors, or events outside our control.</li>
-        <li>Risk of loss passes to you once the order is delivered to the carrier</li>
-      </ul>
+          <h2 className="text-[1rem] font-[area] font-extrabold mt-4">
+            9. Third-Party Links
+          </h2>
 
-      <h2 className="text-xl font-semibold mt-4">6. Returns & Refunds</h2>
-      <ul className="list-disc ml-6 mt-2 font-[area] font-[editorial]">
-        <li>You may return eligible items within 30 days of delivery, provided they are unused, in original condition, and in original packaging.</li>
-        <li>Return shipping costs are your responsibility. If returned items arrive damaged, only partial refunds may be issued</li>
-        <li>Damaged or defective items must be reported within 7 days of delivery with supporting photos. Replacements will be offered where possible.</li>
-      </ul>
+          <p className="mt-2 font-[area] text-[1rem]">
+            Our Site may contain links to third-party websites. We do not
+            control or endorse these sites and are not responsible for their
+            content, policies, or practices.
+          </p>
 
-      <h2 className="text-xl font-semibold mt-4">7. Intellectual Property</h2> 
-      <ul className="list-disc ml-6 mt-2 font-[area] font-[editorial]">
-        <li>All intellectual property rights in the Site, products, branding, and original content are owned by Two Zero Club or our licensors.</li>
-        <li>You may not copy, reproduce, distribute, or use any part of our Site or products for commercial purposes without prior written cosent.</li>
-      </ul>
+          <h2 className="text-[1rem] font-[area] font-extrabold mt-4">
+            10. Limitation of Liability
+          </h2>
+          <ul className="list-disc ml-6 mt-4 font-[area] text-[1rem]">
+            <li>
+              The Site and Services are provided “as is” and “as available.” We
+              make no warranties that the Service will be uninterrupted,
+              error-free, or secure.
+            </li>
+            <li>
+              To the fullest extent permitted by law, we are not liable for
+              indirect, incidental, or consequential damages, including lost
+              profits, data, or business opportunities.
+            </li>
+            <li>
+              Nothing in these Terms excludes liability for fraud, death, or
+              personal injury caused by our negligence.
+            </li>
+          </ul>
 
-      <h2 className="text-xl font-semibold mt-4">8. Prohibited Uses</h2> 
-      <ul className="list-disc ml-6 mt-2 font-[area] font-[editorial]">
-        <li>Violate any laws or regulations.</li>
-        <li>Transmit viruses, malware, or harmful code.</li>
-        <li>Infringe on intellectual property rights.</li>
-        <li>Harass, abuse, or defame others.</li>
-        <li>Misuse promotions, referral programs, or discounts.</li>
-      </ul>
+          <h2 className="text-[1rem] font-[area] font-extrabold mt-4">
+            11. Governing Law
+          </h2>
+          <p className="mt-2 font-[area] text-[1rem]">
+            These Terms shall be governed by and construed under the laws of
+            [Insert jurisdiction — e.g., England & Wales or Canada depending on
+            your incorporation]. Disputes shall be subject to the exclusive
+            jurisdiction of the courts of that jurisdiction.
+          </p>
 
-      <h2 className="text-xl font-semibold mt-4">9. Third-Party Links</h2>
-      
-      <p className="mt-2 font-[area] font-[editorial]">
-       Our Site may contain links to third-party websites. We do not control or endorse these sites and are not responsible for their content, policies, or practices.
-      </p>
-
-      <h2 className="text-xl font-semibold mt-4">10. Limitation of Liability</h2>
-      <ul className="list-disc ml-6 mt-4 font-[area] font-[editorial]">
-        <li>The Site and Services are provided “as is” and “as available.” We make no warranties that the Service will be uninterrupted, error-free, or secure.</li>
-        <li>To the fullest extent permitted by law, we are not liable for indirect, incidental, or consequential damages, including lost profits, data, or business opportunities.</li>
-        <li>Nothing in these Terms excludes liability for fraud, death, or personal injury caused by our negligence.</li>
-      </ul>
-
-      <h2 className="text-xl font-semibold mt-4">11. Governing Law</h2>
-      <p className="mt-2 font-[area] font-[editorial]">
-       These Terms shall be governed by and construed under the laws of [Insert jurisdiction — e.g., England & Wales or Canada depending on your incorporation]. Disputes shall be subject to the exclusive jurisdiction of the courts of that jurisdiction.
-      </p>
-
-      <h2 className="text-xl font-semibold mt-4">12. Contact Us</h2>
-      <p className="mt-2 font-[area] font-[editorial]">
-       if you have any questions about these Terms, please contact us at: <b>twozeroclub@gmail.comb</b>      </p>
-      <br /><br />
-
-      {/* Continue other sections here... */}
-    </div>
+          <h2 className="text-[1rem] font-[area] font-extrabold mt-4">
+            12. Contact Us
+          </h2>
+          <p className="mt-2 font-[area] text-[1rem]">
+            if you have any questions about these Terms, please contact us at:{" "}
+            <b>twozeroclub@gmail.comb</b>{" "}
+          </p>
+          <br />
+          <br />
+        </div>
+      </section>
     </>
   );
 }
